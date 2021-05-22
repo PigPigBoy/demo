5fe0e7e4f52650aaac27456ce75f814e2ef4fe06
PigPigBoy <1359862182@qq.com>
2021-05-22 16:06:37 +0800

    初始化项目

---
+++ gateway/src/test/java/cn/edu/hqu/cst/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.hqu.cst.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
